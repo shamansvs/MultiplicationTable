4a7548940ffc4106f299095a941f35ee1a416e0c
Vitalik <shamansvs@gmail.com>
2026-06-30 16:27:43 +0300

    implementation of the int strategy

---
+++ src/main/java/com/vitalik/multiplicationtable/strategy/IntNumberStrategy.java
@@ -0,0 +1,28 @@
+package com.vitalik.multiplicationtable.strategy;
+
+public class IntNumberStrategy implements NumberStrategy<Integer> {
+    @Override
+    public Integer parse(String value) {
+        return Integer.parseInt(value);
+    }
+
+    @Override
+    public Integer add(Integer first, Integer second) {
+        return first + second;
+    }
+
+    @Override
+    public Integer multiply(Integer first, Integer second) {
+        return first * second;
+    }
+
+    @Override
+    public int compare(Integer first, Integer second) {
+        return Integer.compare(first, second);
+    }
+
+    @Override
+    public String format(Integer value) {
+        return String.valueOf(value);
+    }
+}
